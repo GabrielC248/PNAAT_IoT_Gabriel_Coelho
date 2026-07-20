039721d812da0be46d9003bd5449b5fe2e7d8fe8
Gabriel Cavalcanti Coelho <gabrielccoelho10@gmail.com>
2026-07-20 18:33:35 -0300

    feat: configuração básica dos pinos e definições/variáveis principais.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-print("Sistema Kanban Inicializado")
+import machine
+import time
+
+# Constantes do Sistema
+DEBOUNCE_MS = 50    # 50 milissegundos de debouncing
+STOP_MS = 3000      # 3 segundos para alertar a micro-parada
+LUX_THRESHOLD = 500 # Limiar de 500 lux para detecção do objeto
+
+# Definição dos Pinos Utilizados
+BUTTON_PIN = 25
+LDR_DIGITAL_PIN = 26
+LDR_ANALOG_PIN = 27
+
+# Variáveis do Sistema
+part_counter = 0
+
+# Configuração dos Pinos
+reset_btn = machine.Pin(BUTTON_PIN, machine.Pin.IN)
+ldr_digital = machine.Pin(LDR_DIGITAL_PIN, machine.Pin.IN)
+ldr_analog = machine.ADC(machine.Pin(LDR_ANALOG_PIN))
+ldr_analog.atten(machine.ADC.ATTN_11DB)
+
+# Inicialização
+print("Contador de Producao Inicializado")
+
+# Loop Principal
+while True:
+    time.sleep_ms(10) # Pequeno atraso para evitar travamentos no simulador
